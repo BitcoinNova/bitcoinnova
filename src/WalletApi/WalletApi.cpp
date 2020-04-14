eb45a35fe592da80e665eb4d9adbe14f0529cf6d
BitcoinNovaProject <marketing@bitcoinnova.org>
2020-04-14 14:54:06 +0200

    Version v0.14.7
    Add new control points.
    Software activation for the LWMA2 V2 Difficulty Index.
    Small brand changes.

--- src/WalletApi/WalletApi.cpp
+++ src/WalletApi/WalletApi.cpp
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
         std::this_thread::sleep_for(std::chrono::milliseconds(250));
 
         std::cout << "Want documentation on how to use the wallet-api?\n"
-                     "See https://turtlecoin.github.io/wallet-api-docs/\n\n";
+                     "See https://bitcoinnova.github.io/wallet-api-docs/\n\n";
 
         std::string address = "http://" + config.rpcBindIp + ":" + std::to_string(config.port);
 
