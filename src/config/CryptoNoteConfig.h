7058b4a8ca9d415e2f69355ef839f49c21128ad5
BitcoinNovaProject <44268242+BitcoinNovaProject@users.noreply.github.com>
2022-12-05 01:59:02 +0100

    Version v0.28.3

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -108,7 +108,7 @@ namespace CryptoNote
 
         const uint64_t MAXIMUM_MIXIN_V1 = 100;
 
-        const uint64_t MINIMUM_MIXIN_V2 = 7;
+        const uint64_t MINIMUM_MIXIN_V2 = 2;
 
         const uint64_t MAXIMUM_MIXIN_V2 = 7;
 
